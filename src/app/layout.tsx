587fbb0897f0cf63a4af640e0df8c9773a517f8e
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-05-11 13:37:20 +0800

    fix: remote font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
-import { Lusitana } from "next/font/google";
-import "./globals.css";
-
-const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
+import './globals.css';
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${lusitana.className} antialiased`}>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
